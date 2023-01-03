a87092fc220076910a2a8149762041e1c431fd1c
isapunto <119008897+isapunto@users.noreply.github.com>
2023-01-03 14:05:25 +0100

    bucleContrasenia()
    Creamos un bucle ideal para do{}while();.
    Para casa: El mensaje enviado al escribir la primera vez la confirmación de la contraseña, es distinto al mensaje enviado cuando ya existe el error y hay que volver a pedirla.
    Ej: Introduce la confirmación: *****. Te has equivocado. Vuelve a introducir la confirmación: *****. Ahora si es correcta.

--- inicio/dia3_bucles.java
+++ inicio/dia3_bucles.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
 
 public class dia3_bucles{
+  public static void bucleContrasenia(String contri){
+    Scanner sc = new Scanner(System.in);
+    String confirm;
+
+    /*
+    confirm = "";
+    for (; !contri.equals(confirm) ;){
+      System.out.println("Escribe la confirmacion de la contrasenia: ");
+      confirm = sc.nextLine();
+    }
+    */
+
+    /*
+    confirm = "";
+    while (!contri.equals(confirm)){
+      System.out.println("Escribe la confirmacion de la contrasenia: ");
+      confirm = sc.nextLine();
+    }
+    */
+
+    do {
+      System.out.println("Escribe la confirmacion de la contrasenia: ");
+      confirm = sc.nextLine();
+    } while (!contri.equals(confirm));
+  }
   public static void bucleContador(int numero){
     // for  (creamos el contador. Solo una vez ; Condición ; Lo que
     //      queremos que ocurra en cada iteración.)
@@ -26,18 +51,46 @@ public class dia3_bucles{
     //En este caso lo ideal es utilizar el BUCLE FOR.
 
   }
-  // Bucle creciente de 0 hasta el número introducido, de dos en dos.
-  public static void bucleContadorDe2En2HastaNumero(int numero){}
-  // Bucle decreciente de 100 al número menor que 100 introducido.
-  // EXTRA: Controlar que el número sea menor que 100.
-  public static void bucleContadorDe100AlNumero(int numero){}
+  public static void bucleContadorDe2En2HastaNumero(int num){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Introduce un numero: ");
+    num = sc.nextInt();
+
+    for (int i = 0; i <= num; i += 2) {
+      System.out.println(i);
+    }
+  }
+  public static void bucleContadorDe100AlNumero(int num){
+    if (num < 100) {
+      for (int i = 100; i >= num; i--) {
+          System.out.println(i);
+      }
+    } else {
+      System.out.println("El numero debe ser menor que 100");
+    }
+  }
+  public static void bucleContadorDe100AlNumeroV2(int num){
+    if (num>100){
+      num = 0;
+    }
+    for (int i=100 ; i>num ; i--){
+      System.out.println(i);
+    }
+  }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String contrasenia;
     int numero;
 
     System.out.println("Introduce un numero: ");
     numero = Integer.parseInt(sc.nextLine());
-    bucleContador(numero);
+    //bucleContador(numero);
+    //bucleContadorDe100AlNumeroV2(numero);
+    //bucleContadorDe2En2HastaNumero(numero);
+    System.out.println("Introduce una contrasenia: ");
+    contrasenia = sc.nextLine();
+    bucleContrasenia(contrasenia);
 
   }
 
